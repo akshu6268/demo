6d837dfa1cfc1e3dfdec3c6dea2cd3d50351a6cb
akshu6268 <akshatkeshari68@gmail.com>
2024-12-05 21:38:59 +0530

    we are pushing new java code

---
+++ lecture11a.java
@@ -0,0 +1,18 @@
+
+    import java.util.Scanner;
+public class lecture11a {
+    public static void main(String[] args) {
+      Scanner obj=new Scanner(System.in);
+      System.out.print("please enter size of array: ");
+        int size=obj.nextInt();
+        int x[]=new int[size];
+        System.out.print("enter the value: ");
+        for(int i=0; i<size; i++){
+            x[i]=obj.nextInt();
+        }
+        System.out.print("output of array. ");
+        for(int a=0; a<size; a++){
+            System.out.print(x[a]+" ");
+        }
+    }
+}
